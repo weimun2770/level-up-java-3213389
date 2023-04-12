910898c35c3ea79d8141b7065486a7fda6bcbe45
Weimun <42116046+weimun2770@users.noreply.github.com>
2023-04-12 01:00:54 +0000

    implement 100DaysFromNow

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,25 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
+    public static String afterDay(int days) {
+        DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, days);
+
+        return dateFormat.format(calendar.getTime());
+    }
 
     public static void main( String[] args )
     {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+        System.out.println("100 days from now is... " + 
+            afterDay(100));
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,6 +1,11 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import org.junit.Test;
 
@@ -15,6 +20,13 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        // DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy");
+
+        // Calendar dateAfter10Days = Calendar.getInstance();
+        // dateAfter10Days.set(2023, Calendar.APRIL, 21);
+        // assertEquals(dateFormat.format(dateAfter10Days.getTime()), App.afterDay(10));
+
+        // Calendar dateAfter20Days = Calendar.getInstance();
+        // dateAfter10Days.set(2023, Calendar.APRIL, 21);
     }
 }
