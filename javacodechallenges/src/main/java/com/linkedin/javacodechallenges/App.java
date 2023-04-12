e7bc7b8a3d555dd7eba368dc2a3e3b020cb08b53
Weimun <42116046+weimun2770@users.noreply.github.com>
2023-04-12 08:42:09 +0000

    implement JokeGenerator

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,46 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Optional;
+
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
+
+    public static Optional<String> parseJoke(String responseBody) {
+        try {
+            JokeResponse jokeResponse = new Gson().fromJson(responseBody, JokeResponse.class);
+            String joke = jokeResponse.getJoke();
+            if(joke != null) {
+                return Optional.of(jokeResponse.getJoke());
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         // TODO: Call https://icanhazdadjoke.com/ API and display joke
+
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(URI.create("https://icanhazdadjoke.com/"))
+                                .header("accept", "application/json")
+                                .build();
+
+        var response = client.send(request, BodyHandlers.ofString());
+
+        Optional<String> joke = parseJoke(response.body());
+
+        joke.ifPresent(System.out::println);
     }
 }
