118b9527e3ee54bcc5938d227439cb11b64d5e3a
Weimun <42116046+weimun2770@users.noreply.github.com>
2023-04-12 09:11:00 +0000

    implement RedactSecretMessage

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,12 +1,47 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class App 
 {
     public static void redactTextFile(String fileName,
                                       String[] redactedWordsArray) {
+        if(!fileName.endsWith(".txt")) {
+            System.out.println("Invalid file foramt.");
+            return;
+        }
+        
+        try {
+            File orginalFile = new File(fileName);
+            BufferedReader originalFileReader = new BufferedReader(new FileReader(orginalFile));
 
+            File redactedFile = new File("redacted_"+fileName);
+            BufferedWriter redactedFileWriter = new BufferedWriter(new FileWriter(redactedFile));
+
+            String currentLine = originalFileReader.readLine();
+
+            while(currentLine != null) {
+                for(String redactedWord : redactedWordsArray) {
+                    currentLine = StringUtils.replaceIgnoreCase(currentLine, redactedWord, "REDACTED");
+                }
+
+                redactedFileWriter.append(currentLine).append("\n");
+                currentLine = originalFileReader.readLine();
+            }
+
+            originalFileReader.close();
+            redactedFileWriter.close();
+
+        } catch (Exception e) {
+            System.out.println("Exception error..." + e);
+        }
     }
 
     public static void main(String[] args) {
