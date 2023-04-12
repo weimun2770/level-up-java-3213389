e9b5fd67eef8473d027bde768edefec742d773d3
Weimun <42116046+weimun2770@users.noreply.github.com>
2023-04-12 02:24:47 +0000

    implement StudentVolunteers

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,14 +1,47 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class App {
     public static List<String> findStudentsWithIncompleteVolunteerEvents(
             List<String> students,
             Map<String, List<String>> attendeesMapping) {
         // TODO: implement function
-        return List.of();
+        Map<String, Integer> studentWithAttendance = students.stream().collect(Collectors.toMap(Function.identity(), value -> 0));
+        
+        /* sample code
+        attendeesMapping.values()
+                        .forEach(list -> list.stream()
+                                        .filter(studentName -> studentWithAttendance.containsKey(studentName))
+                                        .forEach(filteredStudent -> studentWithAttendance.put(filteredStudent, studentWithAttendance.get(filteredStudent)+1)));
+
+        return studentWithAttendance.entrySet().stream()
+                        .filter(map -> map.getValue() < 2)
+                        .map(studentWithIncompleteMap -> studentWithIncompleteMap.getKey())
+                        .toList();
+        */
+        for(String activity : attendeesMapping.keySet()) {
+            List<String> studentsArrayList = new ArrayList<>(students);
+            List<String> attendeesArrayList = new ArrayList<>(attendeesMapping.get(activity));
+            studentsArrayList.retainAll(attendeesArrayList);
+            for(String attendeeName : studentsArrayList) {
+                studentWithAttendance.put(attendeeName, studentWithAttendance.get(attendeeName)+1);
+            } 
+        }
+
+        List<String> studentWithIncomplete = new ArrayList<>();
+        for(String studentName : studentWithAttendance.keySet()) {
+            if(studentWithAttendance.get(studentName) < 2) {
+                studentWithIncomplete.add(studentName);
+            }    
+        }
+
+        return studentWithIncomplete;
     }
 
     public static void main(String[] args) {
